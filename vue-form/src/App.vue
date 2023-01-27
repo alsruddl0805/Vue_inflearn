350d5c00d4416682c7183c36a80bd8efc86e883b
MIN GYEONG KWON <alsruddl0805@blackholic.com>
2023-01-27 17:58:40 +0900

    form axios setting

---
+++ vue-form/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <form v-on:submit="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="text" v-model="password">
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      event.preventDefault();
+      console.log(this.username, this.password)
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      let data = {
+        username: this.username,
+        password: this.password,
+      }
+      axios.post(url, data)
+      .then(function (res) {
+        console.log('res', res)
+      })
+      .catch(function (err) {
+        console.log('err', err)
+      });
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
