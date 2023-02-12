4af6c3b028fbac507073a42f0bdff6ead30667d1
alsruddl0805 <68004793+alsruddl0805@users.noreply.github.com>
2023-02-12 14:51:15 +0900

    일괄 삭제 기능 구현

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        todo footer
+    <div class="clearAllConatiner">
+        <span class="clearAllBtn" @click="clearTodo">Clear all</span>
     </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        clearTodo: function() {
+            localStorage.clear();
+        },
+    },
 }
 </script>
 
-<style>
-
+<style scoped>
+.clearAllConatiner {width: 8.5rem; height: 50px; line-height: 50px; background-color: #fff; border-radius: 5px; margin: 0 auto;}
+.clearAllBtn {display: block; color: #e20303;}
 </style>
