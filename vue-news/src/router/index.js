1bb069a578483ce7d516594cf35d8ac5a8aacf47
alsruddl0805 <68004793+alsruddl0805@users.noreply.github.com>
2023-03-09 22:39:34 +0900

    router set

---
+++ vue-news/src/router/index.js
@@ -0,0 +1,26 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import { NewsView } from '@/views/NewsView'
+import { AskView } from '@/views/AskView'
+import { JobsView } from '@/views/JobsView' 
+
+const routes = [
+    {
+        // path : url
+        // component: url 주소로 이동했을 때 표시될 컴포넌트 
+        path: '/news',
+        component: NewsView,
+    },
+    {
+        path: '/ask',
+        component: AskView,
+    },
+    {
+        path: '/jobs',
+        component: JobsView,
+    },  
+]
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
